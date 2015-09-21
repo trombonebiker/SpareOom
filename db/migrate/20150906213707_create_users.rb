36350eace6b4636c096dcf4851b770e6e88f24a9
Jack Simpson <trombonebiker@gmail.com>
2015-09-21 16:24:06 +0100

    Updated user model

--- db/migrate/20150906213707_create_users.rb
+++ db/migrate/20150906213707_create_users.rb
@@ -4,10 +4,6 @@ class CreateUsers < ActiveRecord::Migration
       t.string :first_name
       t.string :surname
       t.string :password_digest
-      t.float :meat
-      t.float :non_meat
-      t.float :amount_due_ethan
-      t.float :amount_due_others
       t.string :role
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -26,10 +26,8 @@ ActiveRecord::Schema.define(version: 20150906213707) do
   create_table "users", force: true do |t|
     t.string   "first_name"
     t.string   "surname"
-    t.float    "meat"
-    t.float    "non_meat"
-    t.float    "amount_due_ethan"
-    t.float    "amount_due_others"
+    t.string   "password_digest"
+    t.string   "role"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
